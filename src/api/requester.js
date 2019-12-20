b6d03464fe0a7f540d98ccd7a4314a062e334fa0
Graeme <graemster@gmail.com>
2019-12-19 20:56:55 -0600

    change endpoints

--- src/api/requester.js
+++ src/api/requester.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { AsyncStorage } from "react-native";
 
-// const instance = axios.create({
-//   baseURL: "https://graeme-chat-app.herokuapp.com"
-// });
 const instance = axios.create({
-  baseURL: "http://a9ae4881.ngrok.io"
+  baseURL: "https://graeme-chat-app.herokuapp.com"
 });
+// const instance = axios.create({
+//   baseURL: "http://a9ae4881.ngrok.io"
+// });
 
 // before making an authentication request, check to see if jwtoken is already present
 instance.interceptors.request.use(
