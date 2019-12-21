74005be974248be4ccd370f860dbfab127ccbe0e
Graeme <graemster@gmail.com>
2019-12-21 13:29:01 -0600

    try something else

--- src/screens/RoomScreen.js
+++ src/screens/RoomScreen.js
@@ -98,7 +98,7 @@ const RoomScreen = ({ navigation, isFocused }) => {
 
   const renderItemOutside = (item, index) => {
     // console.log("current index is :", index);
-    console.log("index is: ", index);
+    // console.log("index is: ", index);
     return (
       <MessageItem
         content={item.content}
@@ -112,7 +112,7 @@ const RoomScreen = ({ navigation, isFocused }) => {
   // scroll functions
   const scrollToBottom = () => {
     if (scrollViewRef.current.scrollToEnd) {
-      // console.log("SCROLL TO BOTTOM FIRED!");
+      console.log("SCROLL TO BOTTOM FIRED!");
       scrollViewRef.current.scrollToEnd({ animated: true });
     } else {
       // console.log("scrollToBottom failed.");
