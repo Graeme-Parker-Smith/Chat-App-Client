9b4c75bec36136589234aaae256abd17d3d48d2a
Graeme <graemster@gmail.com>
2019-12-28 00:32:21 -0600

    add lcl

--- src/screens/RoomScreen.js
+++ src/screens/RoomScreen.js
@@ -140,7 +140,7 @@ const RoomScreen = ({ navigation, isFocused }) => {
       });
       setTimeout(() => {
         setLoading(false);
-        // console.log("LOADING IS DONE");
+        console.log("LOADING IS DONE");
       }, 100);
     }
   };
