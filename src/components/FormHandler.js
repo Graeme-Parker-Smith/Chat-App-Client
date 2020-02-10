d74009b7f80f86e6a33fab60e99becf78f28a270
Graeme <graemster@gmail.com>
2020-02-09 18:19:02 -0600

    remove unused formHandler vars

--- src/components/FormHandler.js
+++ src/components/FormHandler.js
@@ -1,10 +1,4 @@
-import React, { useContext, useState, useEffect, useRef } from 'react';
-import { View, Image, StyleSheet, Text, FlatList, TouchableOpacity, Dimensions, AppState } from 'react-native';
-import { Button, Input } from 'react-native-elements';
-import { SafeAreaView, NavigationEvents } from 'react-navigation';
-
-import Spacer from '../components/Spacer';
-import LoadingIndicator from '../components/LoadingIndicator';
+import React from 'react';
 import CreateChannelForm from '../components/CreateChannelForm';
 import CreatePrivateChannelForm from '../components/CreatePrivateChannelForm';
 import EditUserForm from '../components/EditUserForm';
@@ -41,7 +35,5 @@ const FormHandler = ({ formState, setFormState, setIsLoading }) => {
 			return setFormState('');
 	}
 };
-
-const styles = {};
 
 export default FormHandler;
