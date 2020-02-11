65f8d9b05b3ddd4867dda5eae7bdeb3cac810655
Graeme <graemster@gmail.com>
2020-02-10 22:15:49 -0600

    Create User Avatar component

---
+++ src/components/UserAvatar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
+const UserAvatar = ({ avatar, handleClick }) => {
+	return (
+		<View>
+			<TouchableOpacity onPress={handleClick}>
+				{avatar ? (
+					<Image source={{ uri: avatar }} style={styles.avatarStyle} />
+				) : (
+					<Entypo name="user" size={20} color="#0af" />
+				)}
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	avatarStyle: {
+		height: 50,
+		width: 50,
+		borderRadius: 25,
+	},
+});
+
+export default UserAvatar;
