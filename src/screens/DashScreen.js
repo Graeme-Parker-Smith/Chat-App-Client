dfc964b012b128a27ad2cc85a5dc254a8419fa9c
Graeme <graemster@gmail.com>
2020-02-23 22:27:24 -0600

    add PendingList

--- src/screens/DashScreen.js
+++ src/screens/DashScreen.js
@@ -18,6 +18,7 @@ import { Entypo, MaterialIcons } from '@expo/vector-icons';
 import UserAvatar from '../components/UserAvatar';
 import UserSearchItem from '../components/UserSearchItem';
 import FriendsList from '../components/FriendsList';
+import PendingList from '../components/PendingList';
 import BlockedList from '../components/BlockedList';
 import WhiteText from '../components/WhiteText';
 
@@ -41,7 +42,7 @@ const DashScreen = ({ navigation }) => {
 		console.log('socket emitting search');
 	};
 
-	const dashMenus = [{ name: 'friends', comp: <FriendsList user={state.currentUser} /> }, { name: 'blocked', comp: <BlockedList user={state.currentUser} /> }];
+	const dashMenus = [{ name: 'friends', comp: <FriendsList user={state.currentUser} /> }, { name: 'pending', comp: <PendingList user={state.currentUser} /> }, { name: 'blocked', comp: <BlockedList user={state.currentUser} /> }];
 
 	return (
 		<View style={styles.container}>
