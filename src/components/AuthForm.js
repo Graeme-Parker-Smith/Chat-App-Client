6963494150384698dc00644c1e2b722712c30305
Graeme <graemster@gmail.com>
2020-03-24 23:58:34 -0500

    update authform

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,66 +1,68 @@
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
-import { Text, Button, Input } from "react-native-elements";
-import Spacer from "./Spacer";
+import React, { useState, useRef } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text, Button, Input } from 'react-native-elements';
+import FadeInView from '../components/FadeInView';
+import BouncyInput from '../components/BouncyInput';
+import Spacer from './Spacer';
 
 const AuthForm = ({ headerText, errorMessage, onSubmit, submitButtonText }) => {
-  const [username, setusername] = useState("");
-  const [password, setPassword] = useState("");
-  const _next = () => {
-    _passwordInput && _passwordInput.focus();
-  };
+	const [username, setusername] = useState('');
+  const [password, setPassword] = useState('');
+  const _usernameInput = useRef();
+  const _passwordInput = useRef();
 
-  return (
-    <>
-      <Spacer>
-        <Text h3>{headerText}</Text>
-      </Spacer>
-      <Input
-        label="username"
-        value={username}
-        autoFocus={true}
-        onChangeText={setusername}
-        autoCapitalize="none"
-        autoCorrect={false}
-        returnKeyType="next"
-        selectTextOnFocus={true}
-        ref={ref => (_usernameInput = ref)}
-        onSubmitEditing={_next}
-        textContentType="none"
-      />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        textContentType="none"
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        returnKeyType="send"
-        selectTextOnFocus={true}
-        ref={ref => (_passwordInput = ref)}
-        onSubmitEditing={() => onSubmit({ username, password })}
-      />
-      {errorMessage ? (
-        <Text style={styles.errorMessage}>{errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button
-          title={submitButtonText}
-          onPress={() => onSubmit({ username, password })}
-        />
-      </Spacer>
-    </>
-  );
+	const _next = () => {
+		_passwordInput && _passwordInput.focus();
+	};
+
+	return (
+		<>
+			<Spacer>
+				<FadeInView>
+					<Text h3>{headerText}</Text>
+				</FadeInView>
+			</Spacer>
+			<BouncyInput
+				placeholder="username"
+				value={username}
+				autoFocus={true}
+				onChangeText={setusername}
+				autoCapitalize="none"
+				autoCorrect={false}
+				returnKeyType="next"
+				selectTextOnFocus={true}
+				ref={_usernameInput}
+				onSubmitEditing={_next}
+				textContentType="none"
+			/>
+			<Spacer />
+			<BouncyInput
+				label="Password"
+				value={password}
+				textContentType="none"
+				onChangeText={setPassword}
+				autoCapitalize="none"
+				autoCorrect={false}
+				returnKeyType="send"
+				selectTextOnFocus={true}
+				ref={_passwordInput}
+				onSubmitEditing={() => onSubmit({ username, password })}
+			/>
+			{errorMessage ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
+			<Spacer>
+				<Button title={submitButtonText} onPress={() => onSubmit({ username, password })} />
+			</Spacer>
+		</>
+	);
 };
 
 const styles = StyleSheet.create({
-  errorMessage: {
-    fontSize: 16,
-    color: "red",
-    marginLeft: 15,
-    marginTop: 15
-  }
+	errorMessage: {
+		fontSize: 16,
+		color: 'red',
+		marginLeft: 15,
+		marginTop: 15,
+	},
 });
 
 export default AuthForm;
