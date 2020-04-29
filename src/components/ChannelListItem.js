af7d2091c71292a059a0a0e9bc28c6dda3fd3be4
Graeme <graemster@gmail.com>
2020-04-28 19:37:45 -0500

    update cli

--- src/components/ChannelListItem.js
+++ src/components/ChannelListItem.js
@@ -1,66 +1,91 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { AntDesign, Entypo } from '@expo/vector-icons';
 import CacheImage from './CacheImage';
 
 const ChannelListItem = ({ item, color }) => {
 	const [showDescription, setShowDescription] = useState(false);
+	const [heightAnim] = useState(new Animated.Value(60));
+
+	const handleHeightAnim = async () => {
+		if (!showDescription) {
+			await Animated.timing(heightAnim, {
+				toValue: 180,
+				duration: 500,
+			}).start();
+			setShowDescription(true);
+		} else {
+			await Animated.timing(heightAnim, {
+				toValue: 60,
+				duration: 500,
+			}).start();
+			setShowDescription(false);
+		}
+	};
 
 	return (
-		<ListItem
-			// badge={<Badge value={item.msgCount ? item.msgCount : 73} />}
-			// # of messages on channel in badge
-			// badge={{
-			// 	value: item.msgCount >= 0 ? item.msgCount : null,
-			// 	badgeStyle: { backgroundColor: item.msgCount >= 0 ? '#0af' : 'transparent' },
-			// }}
-			// value={73}
-			containerStyle={[
-				styles.channel(item.username ? '#036' : color),
-				{ height: showDescription ? 180 : 60, padding: 0 },
-			]}
-			title={item.name ? item.name : item.username}
-			subtitle={
-				showDescription
-					? item.username
-						? `I'm your friend! We're friends, right?`
-						: item.description || 'No Description'
-					: null
-			}
-			titleStyle={styles.title}
-			subtitleStyle={styles.subtitle}
-			rightElement={
-				<TouchableOpacity
-					onPress={() => setShowDescription(!showDescription)}
-					style={{
-						borderLeftWidth: 1,
-						borderLeftColor: 'black',
-						height: showDescription ? 180 : 60,
-						backgroundColor: 'green',
-						width: 40,
-						borderRadius: 10,
-						justifyContent: 'center',
-					}}
-				>
-					<AntDesign
-						name={showDescription ? 'caretup' : 'caretdown'}
-						size={20}
-						color="black"
-						style={{ alignSelf: 'center' }}
-					/>
-				</TouchableOpacity>
-			}
-			leftAvatar={
-				item.avatar ? (
-					<View>
-						<CacheImage uri={item.avatar} style={styles.avatarStyle} />
-					</View>
-				) : (
-					<Entypo name={item.username ? 'user' : 'users'} size={40} color="#0af" />
-				)
-			}
-		/>
+		<Animated.View style={{ height: heightAnim }}>
+			<ListItem
+				// badge={<Badge value={item.msgCount ? item.msgCount : 73} />}
+				// # of messages on channel in badge
+				// badge={{
+				// 	value: item.msgCount >= 0 ? item.msgCount : null,
+				// 	badgeStyle: { backgroundColor: item.msgCount >= 0 ? '#0af' : 'transparent' },
+				// }}
+				// value={73}
+				containerStyle={
+					// { backgroundColor: 'transparent', alignSelf: 'center' }
+					[styles.channel(item.username ? '#036' : color), { height: showDescription ? 180 : 60, justifyContent: 'center', alignItems: 'center', padding: 0 }]
+				}
+				// Component={<Animated.View style={{ height: heightAnim }} />}
+				contentContainerStyle={
+					showDescription
+						? { justifyContent: 'flex-start', height: 140 }
+						: { justifyContent: 'center', height: 20 }
+				}
+				title={item.name ? item.name : item.username}
+				subtitle={
+					showDescription
+						? item.username
+							? `I'm your friend! We're friends, right?`
+							: item.description || 'No Description'
+						: null
+				}
+				titleStyle={styles.title}
+				subtitleStyle={styles.subtitle}
+				rightElement={
+					<TouchableOpacity
+						onPress={handleHeightAnim}
+						style={{
+							borderLeftWidth: 1,
+							borderLeftColor: 'black',
+							height: showDescription ? 180 : 60,
+							backgroundColor: 'green',
+							width: 40,
+							borderRadius: 10,
+							justifyContent: 'center',
+						}}
+					>
+						<AntDesign
+							name={showDescription ? 'caretup' : 'caretdown'}
+							size={20}
+							color="black"
+							style={{ alignSelf: 'center' }}
+						/>
+					</TouchableOpacity>
+				}
+				leftAvatar={
+					item.avatar ? (
+						<View>
+							<CacheImage uri={item.avatar} style={styles.avatarStyle} />
+						</View>
+					) : (
+						<Entypo name={item.username ? 'user' : 'users'} size={40} color="#0af" />
+					)
+				}
+			/>
+		</Animated.View>
 	);
 };
 
