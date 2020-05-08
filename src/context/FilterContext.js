59c46adff0144bec0a6724a5658dee9f162888b3
Graeme <graemster@gmail.com>
2020-05-08 12:17:26 -0500

    finish methods

--- src/context/FilterContext.js
+++ src/context/FilterContext.js
@@ -6,6 +6,12 @@ const filterReducer = (state, action) => {
 	switch (action.type) {
 		case 'toggle_public':
 			return { ...state, public: !state.public };
+		case 'toggle_private':
+			return { ...state, private: !state.private };
+		case 'toggle_mature':
+			return { ...state, mature: !state.mature };
+		case 'set_channel_search':
+			return { ...state, channelSearch: action.payload };
 		case 'clear':
 			return { public: true, private: true, mature: true, channelSearch: '', msgMin: 0, msgMax: null };
 		default:
@@ -17,10 +23,27 @@ const clearFilter = (dispatch) => () => {
 	dispatch({ type: 'clear' });
 };
 
+const togglePublic = (dispatch) => () => {
+	dispatch({ type: 'toggle_public' });
+};
+const togglePrivate = (dispatch) => () => {
+	dispatch({ type: 'toggle_private' });
+};
+const toggleMature = (dispatch) => () => {
+	dispatch({ type: 'toggle_mature' });
+};
+const setChannelSearch = (dispatch) => (newText) => {
+	dispatch({ type: 'set_channel_search', payload: newText });
+};
+
 export const { Provider, Context } = createDataContext(
 	filterReducer,
 	{
 		clearFilter,
+		togglePublic,
+		togglePrivate,
+		toggleMature,
+		setChannelSearch,
 	},
 	{ public: true, private: true, mature: true, channelSearch: '', msgMin: 0, msgMax: null }
 );
