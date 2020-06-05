26e157a437c2b4b458ed01ec24f85bb4de7a513e
Graeme <graemster@gmail.com>
2020-06-05 13:07:03 -0500

    advSwitch

---
+++ src/components/AdvSwitch.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Switch, StyleSheet } from 'react-native';
+
+const AdvSwitch = (props) => {
+  
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default AdvSwitch;
