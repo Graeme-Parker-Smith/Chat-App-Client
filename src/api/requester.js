0d842db71395d47ef8fd4fb8f7eb9becb86b84cd
Graeme <graemster@gmail.com>
2020-06-26 15:22:06 -0500

    downgrade react-native-tab-view to solve bug

--- src/api/requester.js
+++ src/api/requester.js
@@ -5,7 +5,7 @@ import { AsyncStorage } from 'react-native';
 //   baseURL: "https://graeme-chat-app.herokuapp.com"
 // });
 const instance = axios.create({
-	baseURL: 'http://06f858f0d1b7.ngrok.io',
+	baseURL: 'http://8ce3528ec921.ngrok.io',
 });
 
 // before making an authentication request, check to see if jwtoken is already present
