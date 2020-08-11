1310459e1519874df646fbab2a3bc0606d2576a9
Graeme <graemster@gmail.com>
2020-08-10 19:47:29 -0500

    update ngrok

--- src/api/requester.js
+++ src/api/requester.js
@@ -5,7 +5,7 @@ import { AsyncStorage } from 'react-native';
 //   baseURL: "https://graeme-chat-app.herokuapp.com"
 // });
 const instance = axios.create({
-	baseURL: 'http://378b373698f8.ngrok.io',
+	baseURL: 'http://5ec51e041365.ngrok.io',
 });
 
 // before making an authentication request, check to see if jwtoken is already present
