ab3d7e91a0f3e090c72d622d5cf75ad0125f2b2c
Graeme <graemster@gmail.com>
2020-08-27 20:31:12 -0500

    create userProfile page that requests user info on mounting. conditionally render add friend and block buttons

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,33 +1,41 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { View, StyleSheet, Text, Dimensions, Image, TouchableOpacity, TouchableHighlight, Modal } from 'react-native';
 import SocketContext from '../context/SocketContext';
+import { Context as ChannelContext } from '../context/ChannelContext';
 import { Button, Input } from 'react-native-elements';
 import { Context as MessageContext } from '../context/MessageContext';
 import { Entypo, MaterialIcons, AntDesign, Foundation } from '@expo/vector-icons';
 import BouncyInput from './BouncyInput';
+import Spacer from './Spacer';
 import WhiteText from './WhiteText';
 
 const UserProfile = ({ username, source, modalVisible, setModalVisible }) => {
 	const socket = useContext(SocketContext);
+	const { addFriend, unblock, state } = useContext(ChannelContext);
+	const [searchResults, setSearchResults] = useState([]);
+	const [friendState, setFriendState] = useState(null);
+
+	let isFriend;
+	let isPending;
+	let isBlocked;
 
 	useEffect(() => {
 		socket.on('usersearch', ({ results }) => {
-			setSearchResults(results);
+			setSearchResults(results[0]);
+			console.log('friend._id is: ', results[0]._id);
+			if (state.currentUser.friends.some((f) => f._id === results[0]._id)) {
+				setFriendState('isFriend');
+			} else if (state.currentUser.pending.some((f) => f._id === results[0]._id)) {
+				setFriendState('isPending');
+			} else if (state.currentUser.blocked.some((b) => b._id === results[0]._id)) {
+				setFriendState('isBlocked');
+			}
 		});
-	}, [state, userSearch, searchResults]);
+	}, [state, searchResults]);
 
-	useEffect(() => {
-		if (userSearch.length > 0) {
-			console.log('useEffect usersearch fired!!!');
-			socket.emit('usersearch', { currentUser: state.currentUser, searchKey: userSearch });
-		}
-	}, [state, userSearch]);
-
-	const doSearch = () => {
-		if (userSearch.length > 0) {
-			socket.emit('usersearch', { currentUser: state.currentUser, searchKey: userSearch });
-		}
-	};
+	// useEffect(() => {
+	// 	socket.emit('usersearch', { currentUser: state.currentUser, searchKey: username });
+	// }, []);
 
 	const fetchUserInfo = () => {
 		console.log('FETCH user info!', username);
@@ -45,7 +53,9 @@ const UserProfile = ({ username, source, modalVisible, setModalVisible }) => {
 				animationType="none"
 				transparent={true}
 				visible={modalVisible}
-				onShow={fetchUserInfo}
+				onShow={() => {
+					socket.emit('usersearch', { currentUser: state.currentUser, searchKey: username });
+				}}
 				onRequestClose={() => {
 					console.log('Modal has been closed.');
 				}}
@@ -103,13 +113,71 @@ const UserProfile = ({ username, source, modalVisible, setModalVisible }) => {
 										height: 200,
 										width: 200,
 										resizeMode: 'contain',
+										alignSelf: 'center',
 									}}
 								/>
 								<View style={styles.userBox}>
-									{/* <WhiteText>{thisUser.username}</WhiteText>
-									<WhiteText>Created:{thisUser.createdAt}</WhiteText>
-									<WhiteText>Score: {thisUser.msgsSent}</WhiteText> */}
-									{/* on long press user username from messageItem and add friend button */}
+									<WhiteText>{username}</WhiteText>
+									{searchResults ? (
+										<>
+											<WhiteText>Created:{searchResults.createdAt}</WhiteText>
+											<WhiteText>Score: {searchResults.msgsSent}</WhiteText>
+
+											{friendState === 'isFriend' || friendState === 'isPending' ? (
+												<Button
+													containerStyle={styles.modalButton}
+													title={
+														friendState === 'isFriend' ? 'Remove Friend' : 'Cancel Request'
+													}
+													onPress={() =>
+														addFriend({
+															username: state.currentUser.username,
+															friendName: searchResults.username,
+															shouldRemove: true,
+														})
+													}
+												/>
+											) : (
+												<Button
+													containerStyle={styles.modalButton}
+													title="Add Friend"
+													onPress={() =>
+														addFriend({
+															username: state.currentUser.username,
+															friendName: searchResults.username,
+														})
+													}
+												/>
+											)}
+											<Spacer />
+
+											{friendState === 'isBlocked' ? (
+												<Button
+													containerStyle={styles.modalButton}
+													title="Unblock"
+													onPress={() =>
+														unblock({
+															username: state.currentUser.username,
+															friendName: searchResults.username,
+														})
+													}
+												/>
+											) : (
+												<Button
+													containerStyle={styles.modalButton}
+													title="Block"
+													onPress={() =>
+														addFriend({
+															username: state.currentUser.username,
+															friendName: searchResults.username,
+															shouldRemove: true,
+															shouldBlock: true,
+														})
+													}
+												/>
+											)}
+										</>
+									) : null}
 								</View>
 							</View>
 						</View>
@@ -125,6 +193,7 @@ const styles = {
 		margin: 10,
 		marginTop: 5,
 		marginBottom: 5,
+		padding: 5
 	},
 	userBox: {
 		marginTop: 0,
